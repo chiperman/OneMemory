0153ce4ccd85216bf8cc8ce6bfadbe736fc9ba6d
chiperman <450838570@qq.com>
2019-10-30 19:30:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/onememory/Splash.java
@@ -0,0 +1,37 @@
+package com.example.onememory;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+//        // 全屏并且隐藏状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//
+//        // 隐藏标题栏 Activity
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        // 隐藏标题栏 AppCompatActivity
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+        // 匿名Handler创建一个延时的调用
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(Splash.this, add_Subscribe.class);  //从启动动画ui跳转到主界面
+                Splash.this.startActivity(intent);
+                Splash.this.finish(); // 结束启动界面
+            }
+        }, 2000);  // 启动动画持续2秒钟
+    }
+}
---
+++ app/src/main/java/com/example/onememory/add_Subscribe.java
@@ -0,0 +1,117 @@
+package com.example.onememory;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+
+public class add_Subscribe extends Activity implements DatePicker.OnDateChangedListener {
+
+    private TextView tv_date;
+    private TextView show_select;
+    private DatePicker.OnDateChangedListener listener = this;
+    private int year, month, day;
+    //在TextView上显示的字符
+    private StringBuffer date;
+    private Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_subscribe);
+
+        // 1.顶部沉浸式状态栏
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // 2.沉浸式下方的三大金刚
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
+        // 3.顶部状态栏透明
+        // 注意！！！1 和 3 不能同时使用
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+        show_select = findViewById(R.id.select);
+        selectSubMethod();
+
+
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        context = this;
+        date = new StringBuffer();
+        tv_date = findViewById(R.id.tv_date);
+        tv_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (date.length() > 0) { //清除上次记录的日期
+                            date.delete(0, date.length());
+                        }
+                        tv_date.setText(date.append(String.valueOf(year)).append("年").append(String.valueOf(month)).append("月").append(day).append("日"));
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                View dialogView = View.inflate(context, R.layout.dialog_date, null);
+                final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
+
+//                dialog.setTitle("设置日期");
+                dialog.setView(dialogView);
+                dialog.show();
+                //初始化日期监听事件
+                datePicker.init(year, month - 1, day, listener);
+            }
+        });
+    }
+
+
+    public void selectSubMethod() {
+        show_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String[] method = new String[]{"按月订阅", "按季订阅", "按年订阅", "一次性买断"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(add_Subscribe.this);
+
+                builder.setTitle("选择你的订阅方式");
+                builder.setItems(method, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        Toast.makeText(getApplicationContext(), "你选择了" + method[i], Toast.LENGTH_SHORT).show();
+                        show_select.setText(method[i]);
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+
+
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+    }
+}
