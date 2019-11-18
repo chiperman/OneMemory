f47b520675026706fd0655a5cb45c88eba979324
chiperman <450838570@qq.com>
2019-11-18 23:09:11 +0800

    更新了选择框为PickView

--- app/src/main/java/com/example/onememory/addSubscribe/Add_Subscribe.java
+++ app/src/main/java/com/example/onememory/addSubscribe/Add_Subscribe.java
@@ -2,33 +2,45 @@ package com.example.onememory.addSubscribe;
 
 
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
 import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.OptionsPickerView;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.example.onememory.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
-public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedListener, View.OnClickListener {
+public class Add_Subscribe extends Activity implements View.OnClickListener {
 
+    private TimePickerView pvTime;
     private TextView tv_date;       // 订阅时间
     private TextView show_select;   // 订阅类型
     private TextView method_select; // 订阅方式
-    private DatePicker.OnDateChangedListener listener = this;
+    //    private DatePicker.OnDateChangedListener listener = this;
     private int year, month, day;
     //在TextView上显示的字符
     private StringBuffer date;
@@ -56,6 +68,10 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
                         View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
 
+        initTimePicker();
+        tv_date = findViewById(R.id.tv_date);
+        tv_date.setOnClickListener(this);
+
         show_select = findViewById(R.id.select);
         show_select.setOnClickListener(this);
 
@@ -74,38 +90,38 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
         day = calendar.get(Calendar.DAY_OF_MONTH);
         context = this;
         date = new StringBuffer();
-        tv_date = findViewById(R.id.tv_date);
-        tv_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        if (date.length() > 0) { //清除上次记录的日期
-                            date.delete(0, date.length());
-                        }
-                        tv_date.setText(date.append(String.valueOf(year)).append("年").append(String.valueOf(month)).append("月").append(day).append("日"));
-                        dialog.dismiss();
-                    }
-                });
-                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-                final AlertDialog dialog = builder.create();
-                View dialogView = View.inflate(context, R.layout.dialog_date, null);
-                final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
-
-//              dialog.setTitle("设置日期");
-                dialog.setView(dialogView);
-                dialog.show();
-                //初始化日期监听事件
-                datePicker.init(year, month - 1, day, listener);
-            }
-        });
+//        tv_date = findViewById(R.id.tv_date);
+//        tv_date.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//                builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        if (date.length() > 0) { //清除上次记录的日期
+//                            date.delete(0, date.length());
+//                        }
+//                        tv_date.setText(date.append(String.valueOf(year)).append("年").append(String.valueOf(month)).append("月").append(day).append("日"));
+//                        dialog.dismiss();
+//                    }
+//                });
+//                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                final AlertDialog dialog = builder.create();
+//                View dialogView = View.inflate(context, R.layout.dialog_date, null);
+//                final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
+//
+////              dialog.setTitle("设置日期");
+//                dialog.setView(dialogView);
+//                dialog.show();
+//                //初始化日期监听事件
+//                datePicker.init(year, month - 1, day, listener);
+//            }
+//        });
     }
 
 //    *****************中间弹框式*****************
@@ -133,14 +149,14 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
 
 
     //    *****************底部式*****************
-    public void selectSubMethod() {
-        show_select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSelectPickerView();
-            }
-        });
-    }
+//    public void selectSubMethod() {
+//        show_select.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showSelectPickerView();
+//            }
+//        });
+//    }
 
     private void showSelectPickerView() {
         // 要展示的数据
@@ -155,7 +171,7 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
             }
         })
                 .setSelectOptions(0)//设置选择第一个
-                .setOutSideCancelable(false)//点击背的地方不消失
+                .setOutSideCancelable(true)//点击背的地方不消失
                 .build();//创建
         // 把数据绑定到控件上面
         pvOptions.setPicker(listData);
@@ -173,6 +189,7 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
         return list;
     }
     //    *****************底部式弹窗截止*****************
+
 
 //    *****************中间弹框式*****************
 //    public void methodSubMethod() {
@@ -198,14 +215,14 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
 //    *****************中间弹框式截止*****************
 
     //    *****************底部式*****************
-    public void methodSubMethod() {
-        method_select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showMethodPickerView();
-            }
-        });
-    }
+//    public void methodSubMethod() {
+//        method_select.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showMethodPickerView();
+//            }
+//        });
+//    }
 
     public void showMethodPickerView() {
         // 要展示的数据
@@ -220,7 +237,7 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
             }
         })
                 .setSelectOptions(0)//设置选择第一个
-                .setOutSideCancelable(false)//点击背的地方不消失
+                .setOutSideCancelable(true)//点击背的地方不消失
                 .build();//创建
         // 把数据绑定到控件上面
         pvOptions.setPicker(listData);
@@ -240,13 +257,76 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
     }
     //    *****************底部式弹窗截止*****************
 
-    @Override
-    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-        this.year = year;
-        this.month = monthOfYear;
-        this.day = dayOfMonth;
+//    @Override
+//    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//        this.year = year;
+//        this.month = monthOfYear;
+//        this.day = dayOfMonth;
+//    }
+
+    private void initTimePicker() {//Dialog 模式下，在底部弹出
+        Calendar selectedDate = Calendar.getInstance();
+        Calendar startDate = Calendar.getInstance();
+        Calendar endDate = Calendar.getInstance();
+        startDate.set(2010, 0, 1);
+        pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date, View v) {
+//                Toast.makeText(Add_Subscribe.this, getTime(date), Toast.LENGTH_SHORT).show();
+                Log.i("pvTime", "onTimeSelect");
+                tv_date.setText(getTime(date));
+            }
+        })
+                .setTimeSelectChangeListener(new OnTimeSelectChangeListener() {
+                    @Override
+                    public void onTimeSelectChanged(Date date) {
+                        Log.i("pvTime", "onTimeSelectChanged");
+                    }
+                })
+                .setType(new boolean[]{true, true, true, false, false, false})
+                .isDialog(true) //默认设置false ，内部实现将DecorView 作为它的父控件。
+                .addOnCancelClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Log.i("pvTime", "onCancelClickListener");
+                    }
+                })
+//                .setSubmitColor(Color.parseColor("#ea4c89"))
+//                .setTitleBgColor(0xFF666666)
+//                .setBgColor(0xFF333333)
+                .setItemVisibleCount(5) // 选项可见数目，若设置偶数，实际值会加1（比如设置6，则最大可见条目为7）
+                .setLineSpacingMultiplier(2.0f) // 每个选项行间距
+                .setDate(selectedDate)
+                .setRangDate(startDate, selectedDate)
+//                .isAlphaGradient(true)
+                .build();
+
+        Dialog mDialog = pvTime.getDialog();
+        if (mDialog != null) {
+
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    Gravity.BOTTOM);
+
+            params.leftMargin = 0;
+            params.rightMargin = 0;
+            pvTime.getDialogContainerLayout().setLayoutParams(params);
+
+            Window dialogWindow = mDialog.getWindow();
+            if (dialogWindow != null) {
+                dialogWindow.setWindowAnimations(com.bigkoo.pickerview.R.style.picker_view_slide_anim);//修改动画样式
+                dialogWindow.setGravity(Gravity.BOTTOM);//改成Bottom,底部显示
+                dialogWindow.setDimAmount(0.3f);
+            }
+        }
     }
 
+    private String getTime(Date date) {//可根据需要自行截取数据显示
+        Log.d("getTime()", "choice date millis: " + date.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
+    }
 
     @Override
     public void onClick(View v) {
@@ -257,10 +337,15 @@ public class Add_Subscribe extends Activity implements DatePicker.OnDateChangedL
             case R.id.sub_add:
                 onBackPressed();
                 break;
+            case R.id.tv_date:
+                pvTime.show(v);
+                break;
             case R.id.select:
-                selectSubMethod();
+                showSelectPickerView();
+                break;
             case R.id.method:
-                methodSubMethod();
+                showMethodPickerView();
+                break;
             default:
                 break;
         }
