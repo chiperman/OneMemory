53c2676e75efe2f65e1918511be9cd4d36e73376
chiperman <450838570@qq.com>
2019-12-30 14:05:32 +0800

    :sparkles:增加订阅编辑按钮及功能

--- app/src/main/java/com/example/onememory/viewCard/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/onememory/viewCard/MyRecyclerViewAdapter.java
@@ -1,15 +1,18 @@
 package com.example.onememory.viewCard;
 
+import android.content.ContentValues;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,6 +25,9 @@ import java.util.ArrayList;
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
     private ArrayList<App> apps;
+    PopupMenu popupMenu;
+    boolean is_outTime, is_descriptionNULL, is_payMethodNULL, is_orderPeriodNULL;
+
 
     public MyRecyclerViewAdapter(ArrayList<App> apps) {
         this.apps = apps;
@@ -30,14 +36,16 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
     class myholder extends RecyclerView.ViewHolder implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
         private LinearLayout card_linearLayout, card_main_linearLayout;
         private ImageView icon;
-        private TextView name, description, price, orderTime,
+        private EditText description, price;
+        private TextView name, orderTime,
                 orderPeriod, payMethod, outTime;
-        private Button btn;
+        public Button btn, card_save;
         // 获取当前App的位置
         public int position;
 
         public myholder(View itemView) {
             super(itemView);
+            card_save = itemView.findViewById(R.id.card_save);
             card_main_linearLayout = itemView.findViewById(R.id.recyclerview_item_main);
             card_linearLayout = itemView.findViewById(R.id.recyclerview_item_card);
             icon = itemView.findViewById(R.id.app_icon);
@@ -57,11 +65,13 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
         public void onClick(View itemView) {
 
             // 创建弹出菜单对象
-            PopupMenu popupMenu = new PopupMenu(itemView.getContext(), btn);
+            popupMenu = new PopupMenu(itemView.getContext(), btn);
             // 填充菜单
             popupMenu.inflate(R.menu.cardmenu);
             // 菜单项点击监听
             popupMenu.setOnMenuItemClickListener(this);
+            // 显示恢复订阅选项
+            if (is_outTime) popupMenu.getMenu().getItem(2).setVisible(true);
             // 显示菜单
             popupMenu.show();
         }
@@ -79,6 +89,86 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
                         ((ViewCard) btn.getContext()).onBackPressed();
                     }
                     break;
+                case R.id.edit_card:
+                    // 设置可编辑状态
+                    description.setFocusableInTouchMode(true);
+                    description.setFocusable(true);
+                    description.requestFocus();
+
+                    price.setFocusableInTouchMode(true);
+                    price.setFocusable(true);
+                    price.requestFocus();
+
+                    orderTime.setFocusableInTouchMode(true);
+                    orderTime.setFocusable(true);
+                    orderTime.requestFocus();
+
+                    orderPeriod.setFocusableInTouchMode(true);
+                    orderPeriod.setFocusable(true);
+                    orderPeriod.requestFocus();
+
+                    payMethod.setFocusableInTouchMode(true);
+                    payMethod.setFocusable(true);
+                    payMethod.requestFocus();
+
+                    card_save.setVisibility(View.VISIBLE);
+                    break;
+
+                case R.id.resume_card:
+//                    ContentValues contentValues = new ContentValues();
+//                    contentValues.put("id",apps.get(getAdapterPosition()).getId());
+//                    contentValues.put("sub_time","0");
+                    // 启用编辑
+                    description.setFocusableInTouchMode(true);
+                    description.setFocusable(true);
+                    description.requestFocus();
+
+                    price.setFocusableInTouchMode(true);
+                    price.setFocusable(true);
+                    price.requestFocus();
+
+                    orderTime.setFocusableInTouchMode(true);
+                    orderTime.setFocusable(true);
+                    orderTime.requestFocus();
+
+                    orderPeriod.setFocusableInTouchMode(true);
+                    orderPeriod.setFocusable(true);
+                    orderPeriod.requestFocus();
+
+                    payMethod.setFocusableInTouchMode(true);
+                    payMethod.setFocusable(true);
+                    payMethod.requestFocus();
+                    String descrip = description.getText().toString();
+                    String money = price.getText().toString();
+                    String sub_time = orderTime.getText().toString();
+                    String sub_period = orderPeriod.getText().toString();
+                    String pay_method = payMethod.getText().toString();
+
+                    ContentValues contentValues = new ContentValues();
+
+                    contentValues.put("id", apps.get(position).getId());
+                    contentValues.put("description", descrip);
+                    contentValues.put("money", money);
+                    contentValues.put("money", money);
+                    contentValues.put("money", money);
+                    contentValues.put("sub_time", sub_time);
+                    contentValues.put("sub_period", sub_period);
+                    contentValues.put("pay_method", pay_method);
+                    MainActivity.getDatabase().update("apps", contentValues, "id=?", new String[]{apps.get(position).getId() + ""});
+
+                    apps.get(position).setDescription(descrip);
+                    apps.get(position).setMoney(Float.parseFloat(money));
+                    apps.get(position).setSubTime(sub_time);
+                    apps.get(position).setSubPeriod(sub_period);
+                    apps.get(position).setPayMethod(pay_method);
+
+                    MainActivity.apps.get(position).setDescription(descrip);
+                    MainActivity.apps.get(position).setMoney(Float.parseFloat(money));
+                    MainActivity.apps.get(position).setSubTime(sub_time);
+                    MainActivity.apps.get(position).setSubPeriod(sub_period);
+                    MainActivity.apps.get(position).setPayMethod(pay_method);
+
+                    Toast.makeText(description.getContext(), "续订成功！", Toast.LENGTH_SHORT).show();
             }
             return false;
         }
@@ -92,24 +182,62 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final RecyclerView.ViewHolder holder, final int position) {
         ((myholder) holder).card_main_linearLayout.setBackgroundColor(Color.parseColor(apps.get(position).getBgColor()));
         // 背景颜色透明度
         ((myholder) holder).card_main_linearLayout.getBackground().setAlpha(215);
         ((myholder) holder).card_linearLayout.setBackgroundColor(Color.parseColor(apps.get(position).getBgColor()));
         ((myholder) holder).icon.setImageResource(apps.get(position).getIconId());
+        ((myholder) holder).card_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 修改订阅信息
+                String description = ((myholder) holder).description.getText().toString();
+                String money = ((myholder) holder).price.getText().toString();
+                String sub_time = ((myholder) holder).orderTime.getText().toString();
+                String sub_period = ((myholder) holder).orderPeriod.getText().toString();
+                String pay_method = ((myholder) holder).payMethod.getText().toString();
+
+                ContentValues contentValues = new ContentValues();
+
+                contentValues.put("id", apps.get(position).getId());
+                contentValues.put("description", description);
+                contentValues.put("money", money);
+                contentValues.put("money", money);
+                contentValues.put("money", money);
+                contentValues.put("sub_time", sub_time);
+                contentValues.put("sub_period", sub_period);
+                contentValues.put("pay_method", pay_method);
+                MainActivity.getDatabase().update("apps", contentValues, "id=?", new String[]{apps.get(position).getId() + ""});
+
+                apps.get(position).setDescription(description);
+                apps.get(position).setMoney(Float.parseFloat(money));
+                apps.get(position).setSubTime(sub_time);
+                apps.get(position).setSubPeriod(sub_period);
+                apps.get(position).setPayMethod(pay_method);
+
+                MainActivity.apps.get(position).setDescription(description);
+                MainActivity.apps.get(position).setMoney(Float.parseFloat(money));
+                MainActivity.apps.get(position).setSubTime(sub_time);
+                MainActivity.apps.get(position).setSubPeriod(sub_period);
+                MainActivity.apps.get(position).setPayMethod(pay_method);
+
+                Toast.makeText(((myholder) holder).btn.getContext(), "修改成功！", Toast.LENGTH_SHORT).show();
+            }
+        });
+        is_outTime = apps.get(position).getState() < 0 ? true : false;
+        is_descriptionNULL = apps.get(position).getDescription().equals("") || apps.get(position).getDescription() == null ? false : true;
+        is_payMethodNULL = apps.get(position).getPayMethod().equals("") || apps.get(position).getPayMethod() == null ? false : true;
+        is_orderPeriodNULL = apps.get(position).getSubPeriod().equals("") || apps.get(position).getSubPeriod() == null ? false : true;
 
         // 用户设置属性
-        boolean is_descriptionNULL = apps.get(position).getDescription().equals("") || apps.get(position).getDescription() == null ? false : true;
         if (is_descriptionNULL)
             ((myholder) holder).description.setText(apps.get(position).getDescription());
 
-        boolean is_payMethodNULL = apps.get(position).getPayMethod().equals("") || apps.get(position).getPayMethod() == null ? false : true;
         if (is_payMethodNULL)
             ((myholder) holder).payMethod.setText(apps.get(position).getPayMethod());
         else ((myholder) holder).payMethod.setText("无");
 
-        boolean is_orderPeriodNULL = apps.get(position).getSubPeriod().equals("") || apps.get(position).getSubPeriod() == null ? false : true;
         if (is_orderPeriodNULL)
             ((myholder) holder).orderPeriod.setText(apps.get(position).getSubPeriod());
         else ((myholder) holder).orderPeriod.setText("无");
@@ -121,7 +249,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
 
 
         String outTime_description = "还剩" + apps.get(position).getState() + "天过期";
-        boolean is_outTime = apps.get(position).getState() < 0 ? true : false;
+
         if (is_outTime) ((myholder) holder).outTime.setText(R.string.overdue);
         else ((myholder) holder).outTime.setText(outTime_description);
 
@@ -132,4 +260,6 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter {
     public int getItemCount() {
         return apps.size();
     }
+
+
 }
